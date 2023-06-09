2d8feff8bc2c4d4b813fa0e1983bb69309718332
Dương Quốc Hiếu <73685928+mongjoe@users.noreply.github.com>
2023-06-09 17:50:27 +0700

    lol

--- public/index.js
+++ public/index.js
@@ -1,16 +1,62 @@
-let items = document.querySelectorAll("#recipeCarousel .carousel-item");
+var multipleCardCarousel = $("#carouselExampleControls");
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel[0], {});
+  var carouselWidth = $(".slider-inside")[0].scrollWidth;
+  var cardWidth = $(".slider-group").width();
+  var scrollPosition = 0;
 
-items.forEach((el) => {
-  const minPerSlide = 10;
-  let next = el.nextElementSibling;
-  for (var i = 1; i < minPerSlide; i++) {
-    if (!next) {
-      // wrap carousel by using first child
-      next = items[0];
+  function nextSlide() {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls .slider-inside").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    } else {
+      // Nếu đến ảnh cuối cùng, trở về ảnh đầu tiên
+      scrollPosition = 0;
+      $("#carouselExampleControls .slider-inside").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
     }
-    let cloneChild = next.cloneNode(true);
-    el.appendChild(cloneChild.children[0]);
-    next = next.nextElementSibling;
+  }
+  $("#carouselExampleControls .carousel-control-next").on("click", nextSlide);
+
+  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls .slider-inside").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    } else {
+      // Nếu ở ảnh đầu tiên, trở về ảnh cuối cùng
+      scrollPosition = carouselWidth - cardWidth * 4;
+      $("#carouselExampleControls .slider-inside").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+
+  // setInterval(nextSlide, 6000); // Slide tự động chạy sau mỗi 3 giây
+} else {
+  multipleCardCarousel.addClass("slide");
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Cuộn mượt đến đầu trang
+  });
+}
+
+window.addEventListener("scroll", function () {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.documentElement.scrollTop > 3000) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
   }
 });
-    
